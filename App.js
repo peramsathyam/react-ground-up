5fc2817117c2c2ef5a6cabe76cb7f105fa1a1616
Peramanathan <prem@weavler.it>
2016-01-28 21:19:21 +0100

    Updating dom with state

--- App.js
+++ App.js
@@ -2,24 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { txt: 'this is the state txt'}
+  }
   render() {
-    let txt = this.props.txt // outside we don't need text, hence avoid `var`
-    return <h1>{txt} and cat value is {this.props.cat}</h1>
+    return <h1>state value : {this.state.txt}</h1>
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'this is the default txt'
-}
-
-
 /* Hack-hint: render(<el attr="">, querySelector); el:=tag, attr:=props */
 ReactDOM.render(
-  <App cat={14} />,
+  <App cat={14} />, // props are static values or methods unlike states
   document.getElementById('app')
 );
