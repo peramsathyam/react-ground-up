9ad357ba512dc532618e2658e9f4851be5edc3c4
Peramanathan <prem@weavler.it>
2016-01-28 20:41:57 +0100

    Introduction to properties

--- App.js
+++ App.js
@@ -1,26 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-/* 1. Stateful class Component */
-// class App extends React.Component {
-//   render() {
-//     // return <div>Jai is my sweety</div> or (el, probs, innerHTML)
-//     return React.createElement('h1', null, 'Jai is my sweety') //jsx => js
-//   }
-// }
-
-/* 2. Stateless function Component */
-// const App = () => <h1>Hello Jai kutty! I love you </h1>
-
-/* 3. Bit advanced Component */
 class App extends React.Component {
   render() {
-    return ( // if div start in this line then brackets not needed
-      <div>
-        <h1>My dear jai</h1>
-        <b>You are my soul</b>
-      </div>
-    ); // semi-colon not needed
+    let txt = this.props.txt // outside we don't need text, hence avoid `var`
+    return <h1>{txt}</h1>
   }
 }
 
-export default App
+
+
+/* Hack-hint: render(<el attr="">, querySelector); el:=tag, attr:=props */
+ReactDOM.render(
+  <App txt="This is example probs" />,
+  document.getElementById('app')
+);
