0b6a2497924bfc17916124478d45acc8a4b7b515
Peramanathan <prem@weavler.it>
2016-01-28 21:03:56 +0100

    props are static vales pased in tags unlike states

--- App.js
+++ App.js
@@ -4,14 +4,22 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   render() {
     let txt = this.props.txt // outside we don't need text, hence avoid `var`
-    return <h1>{txt}</h1>
+    return <h1>{txt} and cat value is {this.props.cat}</h1>
   }
 }
 
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt: 'this is the default txt'
+}
 
 
 /* Hack-hint: render(<el attr="">, querySelector); el:=tag, attr:=props */
 ReactDOM.render(
-  <App txt="This is example probs" />,
+  <App cat={14} />,
   document.getElementById('app')
 );
